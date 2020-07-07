4ef707cc0aca466632a082aa7e459ad1acf5d6e9
elvisthermo <elvis.thermo@gmail.com>
2020-07-07 12:05:38 -0300

    finalizando desafio 2

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,64 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+
   }
 
-  public getBalance(): Balance {
-    // TODO
+
+  public getBalance():Balance {
+
+    let outcome = this.getOutcome();
+    let income = this.getIncome();
+
+    const total = income - outcome;
+
+    const balance:Balance = {
+      income,
+      outcome,
+      total
+    }
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  private getIncome():number{
+    let income =[];
+
+    for (const item of this.transactions) {
+      if(item.type==='income'){
+        income.push(item.value);
+      }
+    }
+    if(income.length){
+      const result = income.reduce( (accum,curr) => accum + curr );
+      return result;
+    }
+    return 0;
+  }
+
+  private getOutcome():number{
+    let outcome =[];
+
+    for (const item of this.transactions) {
+      if(item.type==='outcome'){
+        outcome.push(item.value);
+      }
+    }
+    if(outcome.length){
+      const result = outcome.reduce( (accum,curr) => accum + curr );
+      return result;
+    }
+    return 0;
+  }
+
+  public create({title,value,type}:CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title,value,type});
+
+    this.transactions.push(transaction);
+
+
+    return transaction;
   }
 }
 
